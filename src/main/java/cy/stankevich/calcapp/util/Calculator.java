b3381a9b83d6bfed470597b8a24fda5f323ffdc9
Виталий Станкевич <vitaliy.v.stankevich>
2024-10-13 14:34:55 +0300

    with float numbers and no idea folder

--- src/main/java/cy/stankevich/calcapp/util/Calculator.java
+++ src/main/java/cy/stankevich/calcapp/util/Calculator.java
@@ -1,21 +1,13 @@
 package cy.stankevich.calcapp.util;
 
-import java.util.Scanner;
-
 public class Calculator {
-
-    public static int sum(int num1, int num2) {
-        int resultsum = num1 + num2;
-        return resultsum;
+    public static float sum(float num1, float num2, char operation) {
+        return num1 + num2;
     }
-
-    public static int deduct(int num1, int num2) {
-        int resultdeduct = num1 - num2;
-        return resultdeduct;
+    public static float deduct(float num1, float num2, char operation) {
+        return num1 - num2;
     }
-
-    public static float multyply(float num1, float num2) {
-        float resultmultyply = num1 * num2;
-        return resultmultyply;
+    public static float multyply(float num1, float num2, char operation) {
+        return num1 * num2;
     }
 }
