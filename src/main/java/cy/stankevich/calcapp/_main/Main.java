421f2d42f06c2b20ead5ea6d3a8d8ccd95d31b78
Виталий Станкевич <vitaliy.v.stankevich>
2024-10-13 14:34:55 +0300

    with float numbers and no idea folder

--- src/main/java/cy/stankevich/calcapp/_main/Main.java
+++ src/main/java/cy/stankevich/calcapp/_main/Main.java
@@ -2,24 +2,29 @@ package cy.stankevich.calcapp._main;
 
 import cy.stankevich.calcapp.service.ResultWriterService;
 import cy.stankevich.calcapp.util.Calculator;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter two numbers:");
         System.out.println("Enter Number 1:");
-        int num1 = scan.nextInt();
+        float num1 = scan.nextFloat();
         System.out.println("Enter Number 2:");
-        int num2 = scan.nextInt();
+        float num2 = scan.nextFloat();
         System.out.println("Enter an operation you want to make (+, - or *):");
         char operation = scan.next().charAt(0);
-        int resultsum = Calculator.sum(num1, num2);
-        ResultWriterService.printResult(operation "+", num1, num2, resultsum);
-        int resultdeduct = Calculator.deduct(num1, num2);
-        ResultWriterService.printResult(operation "-", num1, num2,  num1, num2, resultdeduct);
-        int resultmultyply = Calculator.multyply(num1, num2);
-        ResultWriterService.printResult(operation "*", num1, num2, resultmultyply);
+        if (operation == '+') {
+            float resultsum = Calculator.sum(num1, num2, operation);
+            ResultWriterService.printResult(operation, num1, num2, resultsum);
+        }
+        if (operation == '-') {
+            float resultdeduct = Calculator.deduct(num1, num2, operation);
+            ResultWriterService.printResult(operation, num1, num2, resultdeduct);
+        }
+        if (operation == '*') {
+            float resultmultyply = Calculator.multyply(num1, num2, operation);
+            ResultWriterService.printResult(operation, num1, num2, resultmultyply);
+        }
     }
 }
--- src/main/java/cy/stankevich/calcapp/service/ResultWriterService.java
+++ src/main/java/cy/stankevich/calcapp/service/ResultWriterService.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import static cy.stankevich.calcapp.util.Calculator.*;
 
 public class ResultWriterService {
-    public static void printResult(String operation, int num1, int num2, int result) {
-        System.out.println(num1+ " " + operation + " " + num2 + " = " + result);
+    public static void printResult(char operation, float num1, float num2, float result) {
+        System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
     }
 }
